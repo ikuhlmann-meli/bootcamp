f4c2b95f25fe351f1b7e0bee607c3366fb1a1688
Iberê Abondanza Kuhlmann <ibere.kuhlmann@mercadolivre.com>
2022-01-12 14:53:01 -0300

    finalizando

--- radix-sort/src/RadixSort.java
+++ radix-sort/src/RadixSort.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,40 +14,47 @@ public class RadixSort {
             strArr[i] = ((Integer) iArr[i]).toString();
         }
 
-        for (int i = 0; i < iArr.length; i++){
-            strArr[i] = leftPad(strArr[i], greatestLength(strArr));
+        int greatestLength = greatestLength(strArr);
 
+        for (int i = 0; i < iArr.length; i++){
+            strArr[i] = leftPad(strArr[i], greatestLength);
             System.out.println(strArr[i]);
         }
 
-        LinkedList[] L = new LinkedList[10];
+        List <List<String>> listas = new ArrayList<List<String>>(10);
 
+        // populando listas
+        for (int i = 0; i <= 9; i++){
+            listas.add(new ArrayList<String>());
+        }
 
+        for (int position = greatestLength - 1; position >= 0; position--){
 
+            for (int i = 0; i <= 9; i++){
+                listas.get(i).clear();
+            }
 
+            for(String str: strArr){
+                int digit = Integer.valueOf(str.substring(position,position + 1));
+                listas.get(digit).add(str);
+            }
 
+            int cont = 0;
+            for (int i = 0; i <=9; i++){
 
+               for (String str: listas.get(i)){
+                   //System.out.println(str);
+                   strArr[cont] = str;
+                   cont ++;
+               }
+            }
+        }
 
+        System.out.println("\n");
 
-
-//        LinkedList[] list = new LinkedList[6];
-//        for (int i = 0; i < 6; i++) {
-//            if (list[i] == null) {
-//                list[i] = new LinkedList();
-//                int temp = i;
-//                for (int j = 0; j < temp + 1; j++) {
-//                    list[i].add(j);
-//                }
-//            }
-//            System.out.print(list[i]);
-//        }
-
-
-
-
-
-
-
+        for (int i = 0; i < iArr.length; i++){
+            System.out.println(strArr[i]);
+        }
 
     }
 
